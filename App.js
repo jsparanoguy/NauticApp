3336f6881bca19abeb4db779b34e984c9edbc823
MisterL33 <laurent.julienne@ynov.com>
2018-02-23 11:47:05 +0100

    fix: homepage

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ import React, { Component } from 'react';
 
 import {  Platform,  StyleSheet,  Text,  View} from 'react-native';
 
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, NavigationAction } from 'react-navigation';
 
 import RootStack from './src/router';
 
@@ -24,7 +24,7 @@ import RootStack from './src/router';
 export default class App extends React.Component {
 
   render() {
-    return <RootStack/>;
+  return (<RootStack/>);
   }
 
 }
--- src/screens/home.js
+++ src/screens/home.js
@@ -21,11 +21,7 @@ export default class HomeScreen extends React.Component {
     const{ navigate } = this.props.navigation
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-        title='login'
-        onPress={() => this.props.navigation.navigate('Login')}
-        />
+  
 
 <View style ={styles.container}>
        
